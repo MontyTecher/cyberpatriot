bc28a4c10af6f4555efddcb734590659545bbb56
null@localhost <null@localhost>
2025-09-26 13:42:45 -0400

    small tweaks

--- main.bash
+++ main.bash
@@ -9,10 +9,10 @@ apt-get install ufw -y && ufw enable
 sed -i 's/\(pam_pwquality.so\).*$/\1 minlen=14/' /etc/pam.d/common-password
 sed -i 's/nullok//' /etc/pam.d/common-auth
 sed -i "s/^\(PASS_MAX_DAYS\).*$/\1\t30/" /etc/login.defs
-sed -i "s/PermitRootLogin no//" /etc/ssh/ssh_config
-echo "PermitRootLogin no" >> /etc/ssh/ssh_config
 
-find /home -type f \( -name "*.tar.gz" -o -name "*.tgz" -o -name "*.zip" -o -name "*.deb" -o -name "*.mp3" -o -name "*.mp4" \) >> ./log
+prohibited_files=$(find /home -type f \( -name "*.tar.gz" -o -name "*.tgz" -o -name "*.zip" -o -name "*.deb" -o -name "*.mp3" -o -name "*.mp4" \))
+echo $prohibited_files >> ./log
+rm -f $prohibited_files
 mawk -F: '$1 == "sudo"' /etc/group >> ./log
 
 chmod 600 /etc/shadow
