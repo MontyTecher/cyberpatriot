db9c8910545d3579ba697911d021c544005b5220
null@localhost <null@localhost>
2025-09-26 13:00:51 -0400

    rewrote script

--- main.bash
+++ main.bash
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get purge wireshark wireshark-qt \
-	ophcrack ophcrack-cli
-sudo apt-get install ufw -y && ufw enable
+[ -e './log' ] && rm -f log
 
-mawk -F: '$1 == "sudo"' /etc/group
-mawk -F: '$3 > 999 && $3 < 65534 {print $1}' /etc/passwd
-mawk -F: '$2 == ""' /etc/passwd
-mawk -F: '$3 == 0 && $1 != "root"' /etc/passwd
+apt-get update -y && apt-get upgrade -y
+apt-get purge wireshark-qt wireshark ophcrack
+apt-get install ufw -y && ufw enable
 
-prohibited_files=`sudo find / -iname '*.mp3' 2> /dev/null`
-echo $prohibited_files
-rm -f $prohibited_files
+sed -i 's/\(pam_pwquality.so\).*$/\1 minlen=14/' /etc/pam.d/common-password
+sed -i 's/nullok//' /etc/pam.d/common-auth
+sed -i "s/^\(PASS_MAX_DAYS\).*$/\1\t30/" /etc/login.defs
+sed -i "s/PermitRootLogin no//" /etc/ssh/ssh_config
+echo "PermitRootLogin no" >> /etc/ssh/ssh_config
 
+find /home -type f \( -name "*.tar.gz" -o -name "*.tgz" -o -name "*.zip" -o -name "*.deb" -o -name "*.mp3" -o -name "*.mp4" \) >> ./log
+mawk -F: '$1 == "sudo"' /etc/group >> ./log
+
+chmod 600 /etc/shadow
+chmod 640 /boot/grub/grub.cfg
+chmod -R 600 /etc/sudoers.d
+
+apt-get install lynis && lynis audit system
